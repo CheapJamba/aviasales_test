cefc97b822f63ede29e9c2a488f63c809724bb96
Dzhanbolat <sattelizer952@gmail.com>
2021-06-14 08:18:47 +0700

    changed route to data file

--- src/main/java/com/bagandov/aviasales/repository/CSVScanner.java
+++ src/main/java/com/bagandov/aviasales/repository/CSVScanner.java
@@ -4,26 +4,23 @@ import com.bagandov.aviasales.exception.CSVFormatException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class CSVScanner {
 
-    private String resourcePath;
+    private String filePath;
 
     private Pattern pattern;
 
-    public CSVScanner(String resourcePath, String patternString) {
-        this.resourcePath = resourcePath;
+    public CSVScanner(String filePath, String patternString) {
+        this.filePath = filePath;
         this.pattern = Pattern.compile(patternString);
     }
 
     public String[] getRecordById(int id) {
         try {
-            URL resource = getClass().getClassLoader().getResource(resourcePath);
-            File file = new File(resource.toURI());
+            File file = new File(filePath);
             Scanner scanner = new Scanner(file);
             scanner.nextLine();
             while (scanner.hasNextLine()) {
@@ -31,18 +28,16 @@ public class CSVScanner {
                 String[] splits = line.split(",");
                 if (Integer.parseInt(splits[0]) == id) {
                     if (!pattern.matcher(line).matches()) {
-                        throw new CSVFormatException("Строки в файле " + resourcePath +
+                        throw new CSVFormatException("Строки в файле " + filePath +
                                 " должны соответствовать формату: (" + pattern + ")");
                     }
                     return splits;
                 }
             }
-        } catch (URISyntaxException ex) {
-            throw new RuntimeException("Error accessing resource", ex);
         } catch (FileNotFoundException ex) {
             throw new RuntimeException("File not found", ex);
         } catch (NumberFormatException ex) {
-            throw new CSVFormatException("Строки в файле " + resourcePath + " должны соответствовать формату: ("
+            throw new CSVFormatException("Строки в файле " + filePath + " должны соответствовать формату: ("
                     + pattern + ")", ex);
         }
         throw new RuntimeException("File doesn't contain string with id: " + id);
